d3e9c76a043031fe7733e586a09d3792a159f5cf
github username <github email address>
2020-10-07 18:44:47 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)            # put argument(s) here
+  if block_given?
+   i = 0
+  while i < collection.length
+  yield collection[i]
+    i += 1
+  end 
+  collection
+  else
+   "Hey, No block given!"
+  end
+end
+
+
+my_each([1, 2, 3, 4]) do |i| 
+  p i
 end
